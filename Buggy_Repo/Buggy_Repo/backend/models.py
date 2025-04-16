aff8761c967be2d86b2e195b83cb096e6f1ca163
Sahishnu Pawan Kumar <sahishnu.pawankumar@research.iiit.ac.in>
2025-04-16 16:21:35 +0530

    corrected models.py (sahishnu)

--- Buggy_Repo/Buggy_Repo/backend/models.py
+++ Buggy_Repo/Buggy_Repo/backend/models.py
@@ -1,11 +1,19 @@
 from pydantic import BaseModel
+from typing import Optional
 
-class Item():
+class Item(BaseModel):
     name: str #ERROR FIX: changed int to str
     description: str
+    _id: Optional[str] = None
 
 class User(BaseModel):
     username: str
     bio: str
+    _id: Optional[str] = None
+
+class QuizAnswer(BaseModel):
+    id: int
+    answer: str
+    score: int
     
     # You can raise your hands and give the answer to the chocolate question
